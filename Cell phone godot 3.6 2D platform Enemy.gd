2ce86b162020c9a41d8f297fc96da0c4096de927
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-06-02 09:15:28 -0400

    Create Cell phone godot 3.6 2D platform Enemy.gd

---
+++ Cell phone godot 3.6 2D platform Enemy.gd
@@ -0,0 +1,68 @@
+extends KinematicBody2D
+class_name boss
+
+signal add_score
+signal enemy_died
+var ep = 4
+var inout_vector = Vector2.ZERO
+var speed = 1
+var velocity = Vector2(-1,0)
+var score = 0
+var direction = 1
+
+func _physics_process(delta):
+	global_position.x += -speed * direction
+	
+
+func take_damage(damage):
+	$explode.play()
+	ouch()
+	$AnimationPlayer.play("death")
+	ep -= damage
+	ep -= 1
+	emit_signal("enemy_died")
+	$AnimationPlayer.play("death")
+	if ep >= 0:
+		emit_signal("enemy_died")
+		$AnimationPlayer.play("death")
+
+func ouch():
+	emit_signal("add_score")
+	$AnimationpPlayer.play("death")
+	$explode.play()
+	take_damage(1)
+	emit_signal("add_score")
+
+func _on_sides_checker_body_entered(body):
+	if body.get_collision_layer() == 1:
+		$explode.play()
+		body.take_damage(1)
+		body.loadhearts()
+		emit_signal("died")
+		emit_signal("add_score")
+		$Sprite.play("squashed")
+		set_modulate(Color(0.3,0.3,0.3,0.3))
+		enemy_died()
+		$AnimationPlayer.play("death")
+
+func enemy_died():
+	ep == 0
+	if ep <= 0:
+		get_tree().change_scene("res://YouWin.tscn")
+		$Sprite.play("squashed")
+		set_modulate(Color(3,1,1,1))
+		$dieTimer.start()
+
+func _on_Sprite_animation_finished(body):
+	$dieTimer.start()
+	emit_signal('add_score')
+
+func _on_dieTimer_timeout():
+	get_tree().change_scene("res://YouWin.tscn")
+
+func _on_AnimationPlayer_animation_finished(death):
+		emit_signal("add_score")
+
+func _on_sides_checker_area_entered(area):
+	emit_signal("add_score")
+	$Sprite.play("squashed")
