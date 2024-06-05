58a944c841a9645a2630d79e377e93a780ab5571
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-06-05 11:12:37 -0400

    Update Cell phone godot 3.6 2D platform game World.gd

--- Cell phone godot 3.6 2D platform game World.gd
+++ Cell phone godot 3.6 2D platform game World.gd
@@ -3,13 +3,9 @@ extends Node2D
 var score = 0
 
 func score():
-	$gameover.play()
 	score += 1
-	$HUD/Score.text = " SCORE " + str(score)
-	if score >= 100:
+	$HUD/Score.text = " SCORE: " + str(score)
+	if score == 44:
 		get_tree().change_scene("res://YouWin.tscn")
-
-
-
-func _on_gameover_finished():
-	$gameover.stop()
+		score += 1
+		get_tree().change_scene("res://YouWin.tscn")
