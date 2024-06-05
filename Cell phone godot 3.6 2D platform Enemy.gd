c1dc42b3ba208829db05f4f916141e96bcaaafa3
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-06-05 16:53:44 -0400

    Update Cell phone godot 3.6 2D platform Enemy.gd

--- Cell phone godot 3.6 2D platform Enemy.gd
+++ Cell phone godot 3.6 2D platform Enemy.gd
@@ -49,6 +49,7 @@ func _on_sides_checker_body_entered(body):
 func enemy_died():
 	ep == 0
 	if ep <= 0:
+		$AnimationPlayerp.play("death")
 		$Sprite.play("squashed")
 		set_modulate(Color(3,1,1,1))
 		$dieTimer.start()
@@ -60,9 +61,14 @@ func _on_Sprite_animation_finished(body):
 	queue_free()
 	if ep <= 0:
 		enemy_died()
+		body.queue_free()
+	queue_free()
 	$dieTimer.start()
 
 func _on_dieTimer_timeout():
+	ep -= 1
+	if ep <= 0:
+		enemy_died()
 	get_tree().change_scene("res://YouWin.tscn")
 
 func _on_AnimationPlayer_animation_finished(death):
