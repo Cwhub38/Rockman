9da59404c33043f8f1a8189b5aca0b8ab674ee0e
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-06-05 11:11:16 -0400

    Update Cell phone godot 3.6 2D Bullet.gd

--- Cell phone godot 3.6 2D Bullet.gd
+++ Cell phone godot 3.6 2D Bullet.gd
@@ -13,26 +13,24 @@ func _physics_process(delta):
 	
 	if is_on_wall():
 		queue_free()
+		if is_in_group("enemies"):
+			queue_free()
+		if is_in_group("enemies"):
+			queue_free()
 	
 		velocity = move_and_slide(velocity,Vector2.UP)
-
-
 
 func _on_bullet_body_entered(body):
 	if is_in_group("enemies"):
 		emit_signal("add_score")
-
-
+		$exploded.play()
+		queue_free()
 
 func _on_bullet_area_entered(area):
 	if is_in_group("enemies"):
-		 emit_signal("add_score")
-
-
-
-func _on_bullet_body_exited(body):
-	 emit_signal("add_score")
-
+		emit_signal("add_score")
+		$exploded.play()
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
-	 emit_signal("add_score")
+	queue_free()
