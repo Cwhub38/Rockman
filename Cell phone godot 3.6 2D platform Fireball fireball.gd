fbbc56f8884b0085766e2c96b666a70137cd4985
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-06-29 17:07:07 -0400

    Update Cell phone godot 3.6 2D platform Fireball fireball.gd

--- Cell phone godot 3.6 2D platform Fireball fireball.gd
+++ Cell phone godot 3.6 2D platform Fireball fireball.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 class_name Fireball
 
-var velocity= Vector2()
+var velocity = Vector2()
 var direction = 1
 const speed = 800
 
@@ -9,11 +9,21 @@ func _ready():
 	velocity.x = speed * direction 
 
 func _physics_process(delta):
-	
+	velocity = move_and_slide(velocity,Vector2.UP)
 	if is_on_wall():
 		queue_free()
-	
-	velocity = move_and_slide(velocity,Vector2.UP)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_fireball_body_entered(body):
+	if body.is_in_group("enemies"):
+		emit_signal("add_score")
+		$exploded.play()
+		queue_free()
+
+func _on_fireball_area_entered(area):
+	if is_in_group("enemies"):
+		emit_signal("add_score")
+		area.take_damage(1)
+		$exploded.play()
