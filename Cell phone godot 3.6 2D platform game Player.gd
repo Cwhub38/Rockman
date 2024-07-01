5d56f593fddae8bfc6b6f740d5144fa8c2810475
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-07-01 00:44:25 -0400

    Update Cell phone godot 3.6 2D platform game Player.gd

--- Cell phone godot 3.6 2D platform game Player.gd
+++ Cell phone godot 3.6 2D platform game Player.gd
@@ -22,13 +22,14 @@ var hp = 7
 
 func ready():
 	loadhearts()
+	trauma()
 	if hp <= 0:
 		died()
 
 func loadhearts():
 	$"../HUD/Heartsfull".rect_size.x = hp % 53
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("shoot"):
 		$Sprite.play("shoot")
 		shoot_bullet()
@@ -46,7 +47,6 @@ func _physics_process(delta):
 					velocity.x = RUNSPEED
 				else:
 					velocity.x = SPEED
-					$Sprite.play("walk")
 					$Sprite.flip_h = false
 			elif Input.is_action_pressed("move_left"):
 				if Input.is_action_pressed("run"):
@@ -80,13 +80,15 @@ func _physics_process(delta):
 					shoot_bullet()
 				if Input.is_action_just_pressed("fire"):
 						fire_fireball()
+				if Input.is_action_just_pressed("fire"):
+					trauma()
 
 func shoot_bullet():
 	var direction = 1 if not $Sprite.flip_h else -1
 	var f = bullet.instance()
 	f.direction = direction
 	get_parent().add_child(f)
-	f.position.y = position.y + 25 * direction
+	f.position.y = position.y + -45 * direction
 	f.position.x = position.x + 5 * direction
 
 func fire_fireball():
@@ -94,8 +96,8 @@ func fire_fireball():
 	var c = fireball.instance()
 	c.direction = direction
 	get_parent().add_child(c)
-	c.position.y = position.y + -85 * direction
-	c.position.x = position.x + 100 * direction
+	c.position.y = position.y + -45 * direction
+	c.position.x = position.x + 5 * direction
 
 func ouch():
 	set_modulate(Color(1,0.3,0.3,0.3))
@@ -143,3 +145,6 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 func _on_player_area_entered(area):
 	if area.is_in_group("door"):
 		get_tree().change_scene("res://YouWin.tscn")
+
+func _on_player_body_entered(body):
+	body.queue_free()
