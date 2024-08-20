c6c1b12de4d9c9eda9d4b9937378abe5eb78d0d2
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-08-20 13:54:45 -0400

    Update Cell phone godot 3.6 2D platform game World.gd

--- Cell phone godot 3.6 2D platform game World.gd
+++ Cell phone godot 3.6 2D platform game World.gd
@@ -5,7 +5,10 @@ var score = 0
 func score():
 	score += 1
 	$HUD/Score.text = " SCORE: " + str(score)
-	if score == 44:
-		get_tree().change_scene("res://YouWin.tscn")
-		score += 1
+
+func ready():
+	$AnimatedSprite.play("a")
+
+func _on_door_body_entered(area):
+	if is_in_group("Player"):
 		get_tree().change_scene("res://YouWin.tscn")
